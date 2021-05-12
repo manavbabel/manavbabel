8ecac53925e4082f21b078861f6886371efdebed
manavbabel <manavbabel@gmail.com>
2021-05-12 23:02:55 +0100

    Konami code updated 2

--- main.js
+++ main.js
@@ -1,33 +1,57 @@
 /* Function to shift colours when scrolling past the name */
 
-$(function() {
-    $(window).scroll(function () {
-       if ($(this).scrollTop() > 100) {
-          $("#Name").addClass("changeColor");
-          $("#NameText").addClass("changeColor");
-          $("#Scroll").addClass("hide");
-          /*alert("start")*/
-       } else {
-          $("#Name").removeClass("changeColor");
-          $("#NameText").removeClass("changeColor");
-          $("#Scroll").removeClass("hide");
-       }
-    });
- });
+function ScrollChange(event) {
+  var Name = document.getElementById("Name");
+  var NameText = document.getElementById("NameText");
+  var Scroll = document.getElementById("Scroll");
+  var NameBackground = document.getElementById("NameBackground");
 
- /* Konami code functionality */
+  pos = window.pageYOffset;
+  
+  if (pos > 60) {
+    Name.classList.add("changeColor");
+    NameText.classList.add("changeColor");
+    Scroll.classList.add("hide");
+    NameBackground.classList.add("changeColor");
+    document.body.classList.add("changeColor");
+  } else {
+    Name.classList.remove("changeColor");
+    NameText.classList.remove("changeColor");
+    Scroll.classList.remove("hide");
+    NameBackground.classList.remove("changeColor");
+    document.body.classList.remove("changeColor");
+  }
+}
 
- /*
+document.addEventListener("scroll", ScrollChange, {passive:false});
 
-var KonamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"]
+/*
 
- // a variable to remember the 'position' the user has reached so far.
-var KonamiCodePosition = 0;
+ makes nav visible if Name is offscreen 
+
+let NameObserver = new IntersectionObserver(
+  (Name, observer) => {
+    if (Name.intersectionRatio <= 0.5) {
+        document.getElementById("Nav").classList.add("hidden");
+        alert("hidden!")
+      }
+    }, {threshold: 0});
+
+NameObserver.observe(document.getElementById("NameText"));
+*/
+
+/* Konami code functionality */
+
+var KonamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
+
+// a variable to remember the 'position' the user has reached so far.
+  var KonamiCodePosition = 0;
 
 // add keydown event listener
-document.addEventListener('keydown', (e) => {
+document.addEventListener('keydown', Konami);
 
-  // get the key
+function Konami(e) {
+  
   var key = e.key;
 
   // get the value of the required key from the konami code
@@ -47,10 +71,10 @@ document.addEventListener('keydown', (e) => {
   } else {
     KonamiCodePosition = 0;
   }
-});
-
-function ActivateCheats() {
-  alert("cheats activated");
 }
 
-*/
+function ActivateCheats() {
+  document.getElementById("Name").classList.add("cheats");
+  document.getElementById("NameText").classList.add("cheats");
+  document.getElementById("NameBackground").classList.add("cheats");
+}
